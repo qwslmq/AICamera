54430bfa1dc3fe6eeb74b85c207c04f1bf309670
Lokfar <williangattuso@gmail.com>
2018-07-13 17:36:20 +0800

    OCR function complete
    添加了一些可选接口

--- character_recognition/src/main/java/cn/whu/aicamera/character_recognition/Util/ClientUtil.java
+++ character_recognition/src/main/java/cn/whu/aicamera/character_recognition/Util/ClientUtil.java
@@ -9,6 +9,9 @@ import java.util.HashMap;
 
 import cn.whu.aicamera.character_recognition.CharacterRecognition;
 
+/**
+ *
+ */
 public class ClientUtil {
     public static final String APP_ID = "11528113";
     public static final String API_KEY = "Wt5gGEz7XImQpBqjk9aSsYKN";
@@ -28,5 +31,14 @@ public class ClientUtil {
         // 可选：设置log4j日志输出格式，若不设置，则使用默认配置
         // 也可以直接通过jvm启动参数设置此环境变量
         System.setProperty("aip.log4j.conf", "path/to/your/log4j.properties");
+    }
+
+    /**
+     * 检查是否已经获取client实例，如没有则获取
+     */
+    public static void checkClient(){
+        if(null == CharacterRecognition.client){
+            ClientUtil.getClient();
+        }
     }
 }
